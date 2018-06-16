d907b6dd398cbda98a371e34318db1b912fddbc1
mattes <mattesm@zedat.fu-berlin.de>
2018-06-17 01:00:37 +0200

    introduce base class, brownian motion

--- SDEdata/models.py
+++ SDEdata/models.py
@@ -1,25 +1,41 @@
 import numpy as np
 
 class BaseModel(object):
-		
-	self.dim = None	
-	self.integration_step = None
-	self.params = dict()
+    
+    def __init__(self):
+        self.params = locals()
+        pass
+    
+    def integrate(self, X, t, dt):
+        pass
+    
+    def simulate(self, x_init, dt, length, t_init = 0):
+        time = np.arange(t_init,dt*length,dt)
+        if type(x_init) is np.ndarray:
+            x_init = [x_init]
+    
+        dim = x_init[0].shape[-1]          
+        trajs = []
+        for x0 in x_init:
+            traj = np.zeros((length,dim))
+            traj[0,:] = x0
+            for i in range(1,length):
+                traj[i,:] = self.integrate(traj[i-1,:], time[i-1], dt)
+            trajs.append(traj.copy())
+        return trajs,time
+    
+class BrownianMotion(BaseModel):
 
-	#set all vars as properties
-
-	def __init__(self, x_init):
-		""" """
-		#dimensionality check		
-		if x_init.shape[1] != self.dim:
-			#TODO raise error here 
-			pass
-		self._traj = np.array([x_init])
-
-	def integrate(tau, length):
-		#TODO
-
-	
-class UserModel(object):
-	""" """
-	pass
+    def __init__(self,mu=0,cov=1):
+        super(BrownianMotion,self).__init__()
+        self.mu = mu
+        self.cov = cov
+        
+    def integrate(self,X,t,dt):
+        super(BrownianMotion,self).integrate(X,t,dt)
+        if type(self.mu) is not np.ndarray:
+            return X + np.random.normal(loc=self.mu,scale=np.sqrt(self.cov))
+        else:
+            return X + np.random.multivariate_normal(mean=self.mu,cov=self.cov)
+    
+    
